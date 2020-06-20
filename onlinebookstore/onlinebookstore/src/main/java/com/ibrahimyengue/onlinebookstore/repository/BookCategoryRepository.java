b77243136e5e72aaad8d0eb38811bef1a6365da7
Ibrahima <Ibrahima@Ibrahima-PC>
2020-06-20 15:54:54 -0400

    Created Angular project and completed the feature 1.

--- onlinebookstore/onlinebookstore/src/main/java/com/ibrahimyengue/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/onlinebookstore/src/main/java/com/ibrahimyengue/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,9 +2,11 @@ package com.ibrahimyengue.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ibrahimyengue.onlinebookstore.entity.BookCategory;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
